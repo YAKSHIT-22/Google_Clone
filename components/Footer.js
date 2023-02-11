348dab72dfb8ad202854ce5658ac002e8532accc
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-02-11 17:20:14 +0530

    added search params

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div className="h-[10vh] flex justify-center items-center p-4 w-screen bottom-0 text-slate-700 text-xs sm:text-sm">
-        <p>Copyright@ All Right Reserved | Yakshit</p>
+    <div className="h-[10vh] flex justify-center items-center p-4 w-screen bottom-0 text-slate-600 text-xs sm:text-sm">
+        <p>Copyright&copy;{new Date().getFullYear()} All Right Reserved | Yakshit</p>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,18 @@ import Image from 'next/image'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import {SearchIcon, MicrophoneIcon} from '@heroicons/react/solid';
+import { useRouter } from 'next/router';
+import { useRef } from 'react';
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+  function search(event){
+    event.preventDefault();
+    const term = searchInputRef.current.value;
+    if(!term.trim()) return;
+    router.push(`/search?term=${term.trim()}`);
+  }
   return (
     <div >
       <Head>
@@ -18,11 +28,11 @@ export default function Home() {
         <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png" width="300" height="100" objectFit='cover'/>
         <div className="flex items-center justify-center mx-auto  sm:max-w-[75%] md:max-w-[50%] w-full border border-gray-200 rounded-full p-4">   
               <SearchIcon className='h-5 text-gray-500 mr-2' />
-              <input type="text" className='flex-grow focus:outline-none'/>
+              <input ref={searchInputRef} type="text" className='flex-grow focus:outline-none'/>
               <MicrophoneIcon className='h-5 text-gray-500 ml-2'/>
            </div>
          <div className='flex flex-row items-center justify-center gap-6 text-sm mt-2'>  
-        <button className="btn">Google Search</button>
+        <button onClick={search} className="btn">Google Search</button>
         <button className="btn">I'm Feeling Lucky</button>
         </div>
       </form>
