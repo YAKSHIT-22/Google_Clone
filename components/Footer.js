2d61b8bd4d613a5d9113186d8005943b8167b41f
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-02-11 17:00:20 +0530

    added landing page

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="fixed flex justify-center items-center p-4 w-screen bottom-0 text-slate-700 text-xs sm:text-sm">
+    <div className="h-[10vh] flex justify-center items-center p-4 w-screen bottom-0 text-slate-700 text-xs sm:text-sm">
         <p>Copyright@ All Right Reserved | Yakshit</p>
     </div>
   )
--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,8 @@ import User from './User'
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-center  w-screen">
-      <nav className="flex justify-between py-5 px-3 sm:px-4 md:px-5 items-center w-screen text-sm text-gray-700">
+    <header className="flex items-center justify-center w-screen h-[10vh]">
+      <nav className="flex justify-between py-4 px-3 sm:px-4 md:px-6 items-center w-screen text-sm text-gray-700">
         <div className="flex gap-2 sm:gap-4 items-center justify-center">
            <p className="link">About</p>
            <p className="link">Store</p>
